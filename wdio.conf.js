8742eebe3bc0802f8207f79aa3375a1e9111fb68
alensalu <alensalu@gmail.com>
2023-09-12 16:53:15 +0530

    swaglab edited commit

--- wdio.conf.js
+++ wdio.conf.js
@@ -118,19 +118,19 @@ export const config = {
 
         'acceptInsecureCerts': true,
 
-        // 'goog:chromeOptions': {
+        'goog:chromeOptions': {
 
-        //     // args: [
+            args: [
 
-        //     //     '--window-size=1920,1080',
+                '--window-size=1920,1080',
 
-        //     //     '--incognito',
+                '--incognito',
 
-        //     //     // '--headless=new',
+                '--headless=new',
 
-        //     //     '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+                '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
 
-        //     // ],
+            ],
 
         //     excludeSwitches: ['enable-automation'],
 
