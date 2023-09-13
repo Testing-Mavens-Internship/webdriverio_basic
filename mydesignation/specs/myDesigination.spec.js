60ecf6d5fa46db2826629e2e784fb1d8f4b96c75
jesson006 <jessonjose12@gmail.com>
2023-09-13 16:07:42 +0530

    My Desiganation

--- mydesignation/specs/myDesigination.spec.js
+++ mydesignation/specs/myDesigination.spec.js
@@ -5,30 +5,33 @@ firstName = tester1;
 
 describe("My Desiganation Autaoamtion", () => {
     it("Launch the url", async() => {
-        await homePage.openUrl()
+        await homePage.openUrl();
         expect(await homePage.$header().isDisplayed())
-            .withContext("Expected header to be displayed").toBe(true);
-    })
+            .withContext("Expected header to be displayed")
+            .toBe(true);
+    });
 
     it("Click On product and verify navigation", async() => {
         await homePage.clickOnProduct();
         expect(await homePage.$productheader().isDisplayed())
-            .withContext("Expected header to be displayed").toBe(true);
-    })
+            .withContext("Expected header to be displayed")
+            .toBe(true);
+    });
 
     it("Click Add to cart verify navigation", async() => {
-        await cart.addToCart()
+        await cart.addToCart();
         expect(await cart.$cartHeader().isDisplayed())
-            .withContext("expected header to be displayed").toBe(true)
-    })
+            .withContext("expected header to be displayed")
+            .toBe(true);
+    });
 
     it("Click on cart contents and checkout and verify navigation", async() => {
-        await cart.clickOnCartContents()
+        await cart.clickOnCartContents();
         expect(await cart.$billingHeader().isDisplayed())
-            .withContext("verify the header").toBe(true)
-    })
+            .withContext("verify the header")
+            .toBe(true);
+    });
     it("User details valiadtions", async() => {
-        await billing.billingValidation('billing_last_name_field', firstName);
-
-    })
-})
+        await billing.billingValidation("billing_last_name_field", firstName);
+    });
+});
