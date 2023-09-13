082835d186dd6297ab980996b359fface181d833
jesson006 <jessonjose12@gmail.com>
2023-09-13 16:02:56 +0530

    My Desiganation

--- mydesignation/page_objects/billing_page.js
+++ mydesignation/page_objects/billing_page.js
@@ -1,8 +1,13 @@
 class Billing {
     constructor() {
-        this.$usereDetails = (id) => $(`//p[@id="${id}"]`)
-        this.$$warnings = () => $$(`//strong[text()]`)
-    }
+            this.$usereDetails = (id) => $(`//p[@id="${id}"]`)
+            this.$$warnings = () => $$(`//strong[text()]`)
+        }
+        /**
+         * enter the user detail value and verify it with warnings
+         * @param {field id} id 
+         * @param {value at place holder} value 
+         */
     async billingValidation(id, value) {
         await this.$usereDetails(id).setValue(value)
 
