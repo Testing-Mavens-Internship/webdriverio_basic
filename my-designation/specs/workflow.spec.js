136eefa975b001f17b376ff557a1e9eac92dcbb8
joyalfrancis23 <joyalfrancis23@gmail.com>
2023-09-13 17:53:33 +0530

    added some more it blocks

--- my-designation/specs/workflow.spec.js
+++ my-designation/specs/workflow.spec.js
@@ -31,54 +31,57 @@ describe("My Designation Automation",()=>{
             .withContext("Expected billing details to be displayed")
             .toBe(true)
     })
-    it("Enter details and verify",async ()=>{
-        let warnings = [];
-        warnings = await billingPage.$$warningMessages().map(item => item.getText());
-        for(let item of warnings){
-            expect(await item.isDisplayed()).withContext("Expecting warning messages").toBe(true)
-        }
+    it("Enter first name and verify display",async ()=>{
 
         await billingPage.enterFirstName()
         expect(await billingPage.$verifyIndividual("Billing First name").isDisplayed())
             .withContext("Expecting first name")
             .toBe(true)
+    })
 
+    it("Enter last name and verify display",async ()=>{
         await billingPage.enterLastName()
         expect(await billingPage.$verifyIndividual("Billing Last name").isDisplayed())
             .withContext("Expecting last name")
-            .toBe(true)
-        
+            .toBe(true)        
+    })
+
+    it("Enter address and verify display",async ()=>{
         await billingPage.enterAddress()
         expect(await billingPage.$verifyIndividual("Billing Street address").isDisplayed())
             .withContext("Expecting address")
-            .toBe(true)
+            .toBe(true)       
+    })
 
+    it("Enter city and verify display",async ()=>{
         await billingPage.enterCity()
             expect(await billingPage.$verifyIndividual("Billing Town / City").isDisplayed())
                 .withContext("Expecting city")
                 .toBe(true)
-        
+    })
+
+    it("Enter state and verify display",async ()=>{
         await billingPage.enterState()
             expect(await billingPage.$verifyIndividual("Billing State").isDisplayed())
                 .withContext("Expecting state")
                 .toBe(true)
-
+    })
+    it("Enter post-code and verify display",async ()=>{
         await billingPage.enterPostCode()
             expect(await billingPage.$verifyIndividual("Billing PIN Code").isDisplayed())
                 .withContext("Expecting post code")
                     .toBe(true)
-
+    })
+    it("Enter phone number and verify display",async ()=>{
         await billingPage.enterPhone()
             expect(await billingPage.$verifyIndividual("Billing Phone").isDisplayed())
                 .withContext("Expecting phone number")
                     .toBe(true)
-
+    })
+    it("Enter email and verfiy display",async ()=>{
         await billingPage.enterEmail()
             expect(await billingPage.$verifyIndividual("Billing Email address").isDisplayed())
                 .withContext("Expecting email address")
                     .toBe(true)
-
     })
-
-    
 })
