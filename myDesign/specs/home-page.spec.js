d77caa120d3c0da62007838e52dba88e867303ee
athiramenon <athira3601@gmail.com>
2023-09-13 16:26:21 +0530

    myDesignation final

--- myDesign/specs/home-page.spec.js
+++ myDesign/specs/home-page.spec.js
@@ -4,41 +4,29 @@ import { home } from "../pageobjects/landing-page.js";
 
 describe ("Mydesignation application automation", () => {
     it ("Load Mydesignation url", async () => {
-        await home.openUrl();
-      expect(await home.$header().isDisplayed())
-        .withContext("Expect header to be displayed")
-        .toBe(true);
-        expect(await home.$nav().isDisplayed())
-        .withContext("Expect navbar to be displayed")
-        .toBe(true);
+      await home.openUrl();
+      expect(await home.$header().isDisplayed()).withContext("Expect header to be displayed").toBe(true);
+      expect(await home.$nav().isDisplayed()).withContext("Expect navbar to be displayed").toBe(true);
     });
 
     it ("Click on product", async () => {
       await home.clickOnProduct();
-      expect(await home.$item().isDisplayed())
-      .withContext("Expect navigation to item page")
-      .toBe(true);
+      expect(await home.$item().isDisplayed()).withContext("Expect navigation to item page").toBe(true);
     });
 
     it("Click on size and add to cart", async () => {
       await home.clickOnsize();
-      expect(await home.$added().isDisplayed())
-      .withContext("Expect item added to cart")
-      .toBe(true);
+      expect(await home.$added().isDisplayed()).withContext("Expect item added to cart").toBe(true);
     });
 
     it("Click on cart icon an verify navigation", async () => {
       await home.clickOnCartIcon();
-      expect(await home.$cartItem().isDisplayed())
-      .withContext("Expect navigation to checkout page")
-      .toBe(true);
+      expect(await home.$cartItem().isDisplayed()).withContext("Expect navigation to checkout page").toBe(true);
     });
 
     it("Click on proceed ", async () => {
       await checkoutPage.clickOnProceed();
-      expect(await checkoutPage.$bill().isDisplayed())
-      .withContext("Expect navigation to billing details page")
-      .toBe(true);
+      expect(await checkoutPage.$bill().isDisplayed()).withContext("Expect navigation to billing details page").toBe(true);
     });
 
     it("Click on place order", async () => {
