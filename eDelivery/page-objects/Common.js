2cb011091be16cafdbf10e512e4c8e71e95bed83
Fathima <fathima_marikar@ymail.com>
2023-09-15 00:55:24 +0530

    added comments

--- eDelivery/page-objects/Common.js
+++ eDelivery/page-objects/Common.js
@@ -1,15 +1,13 @@
-export default class Common{
-
-constructor(){
-
-this.$header=()=>$('//img[@class="img-responsive"]')
-}
-
-async loadPage(){
-    await browser.url('https://edelivery.zoproduct.com/')
+export default class Common {
+  constructor() {
+    this.$header = () => $('//img[@class="img-responsive"]');
+  }
+  /**
+   * Method to load the website
+   */
+  async loadPage() {
+    await browser.url("https://edelivery.zoproduct.com/");
     await browser.maximizeWindow();
-    await this.$header().waitForDisplayed({timeout:4000})
-
-}
-
+    await this.$header().waitForDisplayed({ timeout: 4000 });
+  }
 }
