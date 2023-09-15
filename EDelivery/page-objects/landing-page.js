fef1c8232328d5484af463dbecaf987dfb4c5309
AdhithyaSomaraj <21mp1821@rit.ac.in>
2023-09-15 11:18:22 +0530

    uploading signup function

--- EDelivery/page-objects/landing-page.js
+++ EDelivery/page-objects/landing-page.js
@@ -2,10 +2,42 @@
 
     class LandingPage extends Common{
         constructor(){
-            super();
-        }
+            super(); 
+            this.$header=()=>$(`//img[@class="img-responsive"]`)     // website logo
+            this.$loginButton=()=>$(`//button[@class="btn btn-default ng-binding"]`) //login button
+            this.$loginHeader=(value)=>$(`//div[contains(text(),"${value}")]`) //login header
+            this.$signUpHeader=()=>$(`//div[@class="pop_tle"]`) //signUp header
+            this.$signUpNowButton=()=>$(`//div[@class="sgn_up_btn"]`) //sign up now button
+            this.$signUpFields=(input)=>$(`//input[@id="${input}"]`)
+            this.$VerificationMessageHeader=()=>$(`//div[@class="pop_tle ng-binding"]`)
+            this.$loginButton2=()=>$(`//div[@class="subm_btn"]`)
+        }/**
+         * function for click on login button
+         */
         async clickOnLoginButton(){
+        await  this.$loginButton().click()
+        await browser.pause(3000)
+        }
+        /**
+         * function for clicking the sign uo now button
+         */
+        async clickOnSignUpNow(){
+            await this.$signUpNowButton().click()
+            await this.$signUpNowButton().waitForDisplayed({timeout:5000});
+        }
 
+        async fillSignUpField(firstName,lastName,email,phone,fname,lname,emailid,ph){
+            await this.$signUpFields(firstName).setValue(fname)
+            await this.$signUpFields(lastName).setValue(lname)
+            await this.$signUpFields(email).setValue(emailid)
+            await this.$signUpFields(phone).setValue(ph)
+        }
+
+        async clickLoginButton2()
+        {
+            await this.$loginButton2().click()
+            //await this.$loginButton2().waitForDisplayed({timeout:5000});
+            await browser.pause(3000)
         }
     }
     export const landingPage=new LandingPage()
--- EDelivery/specs/EDelivery.spec.js
+++ EDelivery/specs/EDelivery.spec.js
@@ -1,7 +1,42 @@
 import { landingPage } from "../page-objects/landing-page.js";
+import { faker } from '@faker-js/faker';
+
+
+
+let fname = faker.person.firstName(); 
+let lname = faker.person.lastName();
+let emailid=fname+"@gmail.com";
+let ph = Math.floor(Math.random() * 9000000000) + 1000000000;
+
+let firstName="first_name"
+let lastName="last_name"
+let email="email"
+let phone="tel"
+
+
 
 describe("EDelivery Application:",()=>{
     it("Launch the Website",async()=>{
         await landingPage.launchUrl()
+        expect(await landingPage.$header().isDisplayed()).withContext("expect the website logo").toBe(true)
     })
+
+    it("Click on the Login Button",async()=>{
+        await landingPage.clickOnLoginButton()
+        expect(await landingPage.$loginHeader("Login").isDisplayed()).withContext("expect the login header").toBe(true)
+    })
+
+    it("click on the sign up now button",async()=>{
+        await landingPage.clickOnSignUpNow()
+        expect(await landingPage.$signUpHeader().isDisplayed()).withContext("expect the Register header").toBe(true)
+    })
+    it("Fill the sign up fields",async()=>{
+        await landingPage.fillSignUpField(firstName,lastName,email,phone,fname,lname,emailid,ph)
+        expect(await landingPage.$VerificationMessageHeader().isDisplayed()).withContext("Expect the Verification message sent header").toBe(true)
+
+    })
+    it("click on the login button on message verification pop up",async()=>{
+        await landingPage.clickLoginButton2()
+    })
+
 })
